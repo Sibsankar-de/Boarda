e1c2466a9885e76c2a76c657e855a142149e73bf
SambaranBanerjee <sanbaranb25@gmail.com>
2026-02-16 17:29:34 +0530

    Fixed the the file path and dotenv/config import path because this was failing to upload CLOUDINARY ENV

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
-import dotenv from "dotenv";
-import connectDB from "./src/db/index.js";
+//import dotenv from "dotenv";
+import 'dotenv/config';
 import { app } from "./src/app.js";
 
-dotenv.config({
+/*dotenv.config({
     path: './.env'
-});
+});*/
+
+import connectDB from "./src/db/index.js";
 
 const port = process.env.PORT || 8000;
-
+import path from 'path';
+console.log("Looking for .env at:", path.resolve(process.cwd(), '.env'));
 connectDB()
     .then(() => {
         app.listen(port, () => {
